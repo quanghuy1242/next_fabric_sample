039bc4e1e3d8fb157e3ead4b437af4502fa62793
Quang Huy <quanghuy1242@gmail.com>
2019-11-26 17:25:46 +0700

    removed event after done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Router from 'next/router';
 import { Fabric } from 'office-ui-fabric-react';
 
-Router.events.on('routeChangeComplete', () => {
+function removedServerStyle() {
   const ssrStyle = document.querySelector('#ssr-style');
   if (ssrStyle && ssrStyle.parentNode) {
     setTimeout(() => {
       ssrStyle.parentNode.removeChild(ssrStyle);
+      Router.events.off('routeChangeComplete', removedServerStyle);
       console.log('Removed style from server!');
     }, 0);
   }
-});
+}
+Router.events.on('routeChangeComplete', removedServerStyle);
 
 const MyApp = ({ Component, pageProps }) => {
   return (
